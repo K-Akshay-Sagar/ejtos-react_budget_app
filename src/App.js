890935855683761b2d8780d390fbf66772e396af
K-Akshay-Sagar <93024383+K-Akshay-Sagar@users.noreply.github.com>
2023-05-29 20:10:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
                         <Budget />
                     </div>
                     <div className='col-sm'>
-                        <RemainingBudget />
+                        <Remaining />
                     </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
